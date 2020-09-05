d08e6dc38ee208cd1cb3572624550f6e6306f32c
HwangSaw <tinkle9696@gmail.com>
2020-09-05 22:57:54 +0900

    2.7 typename을 하나가 아닌 복수개 사용하자

--- C++STL/C++STL/C++STL.cpp
+++ C++STL/C++STL/C++STL.cpp
@@ -57,8 +57,40 @@ void main()
 	double Char1_SP = 400.25;
 	// 아래 구문은 컴파일러가 int와 double이라는 두가지타입이 있기때문에 현재 Max 로는 T를 어떤걸로 정할지 알수없어서 
 	// no instance of function template "Max" matches the argument list 에러를 발생. 
+	// 2.7의 예제를( typename을 복수개 사용) 통해서 해결가능
 	//double MaxValue1 = Max(Char1_MP, Char1_SP);
 	//cout << "MP와 SP중 가장 큰 값은" << MaxValue1 << "입니다" << endl << endl;
 
 }
 */
+
+/*
+// 2.7 typename 을 하나가 아닌 복수 개 사용하면 된다. 
+#include<iostream>
+using namespace std;
+
+template <typename T1, typename T2> const T1& Max(const T1& a, const T2& b)
+{
+	return a > b ? a : b;
+}
+
+void main()
+{
+	int Char1_MP = 300;
+	double Char1_SP = 400.24;
+	double MaxValue1 = Max(Char1_MP, Char1_SP);
+	cout << "MP와 SP중 가장 큰 값은" << MaxValue1 << "입니다" << endl << endl;
+
+
+	// 기대 값 : MP와 SP중 가장 큰 값은 400.24입니다. 
+	// 실제 값 :  MP와 SP중 가장 큰 값은 400 입니다. 
+	// 기대 값과 실제값이 다른 원인은 typename T1을 반환하는데 T2는 double이기에 int 로 반환시 소수점이 잘린다. 
+	// 그래서 이렇게 서로 다른 타입을 사용하는 경우에는 반환 값을 조심해야한다. 
+}
+*/
+
+
+// /*
+// 2.8 함수 템플릿의 전문화 라는 것이 있다. 
+
+// */
