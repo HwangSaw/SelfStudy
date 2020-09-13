6d3b7f870b6b998c65ac8f2d72d699968a3be163
HwangSaw <tinkle9696@gmail.com>
2020-09-13 22:35:17 +0900

    [C++STL] 3.6 템플릿 파라미터 디폴트 값 사용

--- C++STL/C++STL/C++STL.cpp
+++ C++STL/C++STL/C++STL.cpp
@@ -448,7 +448,7 @@ void main()
 }
 */
 
-// /*
+/*
 // 3.5 클래스 템플릿에서 non-type 파라미터 사용
 
 #include<iostream>
@@ -532,8 +532,91 @@ void main()
 	cout << "스택의 크기는?" << kStack2.GetStackSize() << endl;
 
 }
+ */
+
+// /*
+// 3.6 템플릿 파라미터 디폴트 값 사용
+// 템플릿 파라미터 중 int Size가 non-type 파라미터다.
+// Size의 디폴트 값을 100으로 한다. 
+#include<iostream>
+using namespace std;
+template <typename T, int Size=100>
+class Stack
+{
+public:
+	Stack()
+	{
+		Clear();
+	}
+
+	// 초기화 한다.
+	void Clear()
+	{
+		m_Count = 0;
+	}
+
+	// 스택에 저장된 개수
+	int Count()
+	{
+		return m_Count;
+	}
+
+	// 저장된 데이터가 없는가?
+	bool IsEmpty()
+	{
+		return 0 == m_Count ? true : false;
+	}
+
+	// 데이터를 담을 수 있는 최대 개수
+	int GetStackSize()
+	{
+		return Size;
+	}
+
+	// 데이터를 저장한다.
+	bool push(T data)
+	{
+		// 저장할 수 있는 개수를 넘는지 조사한다.
+		if (m_Count >= Size)
+		{
+			return false;
+		}
+
+		// 저장 후 개수를 하나 늘린다.
+		m_aData[m_Count] = data;
+		++m_Count;
+
+		return true;
+	}
+
+	// 스택에서 빼낸다.
+	T pop()
+	{
+		// 저장된 것이 없다면 0 을 반환한다.
+		if (m_Count < 1)
+		{
+			return 0;
+		}
+
+		// 개수를 하나 감소 후 반환한다.
+		--m_Count;
+		return m_aData[m_Count];
+	}
+
+private:
+	T m_aData[Size];
+	int m_Count;
+};
+
+void main()
+{
+	Stack<int, 64> kStack1;
+	cout << "스택의 크기는?" << kStack1.GetStackSize() << endl;
+
+	Stack<double> kStack2;
+	cout << "스택의 크기는?" << kStack2.GetStackSize() << endl;
+
+}
 // */
-
-
 
 
