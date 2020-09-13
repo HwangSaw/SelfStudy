9dbbb63c354220806251c56ce04d341dd13ae16d
HwangSaw <tinkle9696@gmail.com>
2020-09-13 23:44:35 +0900

    3.8 클래스 템플릿 전문화

--- C++STL/C++STL/C++STL.cpp
+++ C++STL/C++STL/C++STL.cpp
@@ -619,7 +619,7 @@ void main()
 }
  */
 
-// /*
+/*
 // 3.7 스택 클래스의 크기를 클래스 생성자에서 지정
 template<typename T, int Size = 100> class Stack
 {
@@ -707,12 +707,221 @@ void main()
 	cout << "스택의 크기는?" << kStack1.GetStackSize() << endl;
 
 }
+*/
+
+// /*
+
+// 3.8 클래스 템플릿 전문화
+// ID 문자열의 최대 길이 (null 문자 포함 )
+const int MAX_ID_LENGTH = 21;
+
+template<typename T> class Stack
+{
+public:
+	explicit Stack(int size)
+	{
+		m_Size = size;
+		m_aData = new T[m_Size];
+	
+		Clear();
+	}
+
+	~Stack()
+	{
+		delete[] m_aData;
+	}
+
+	// 초기화 한다.
+	void Clear()
+	{
+		m_Count = 0;
+	}
+
+	// 스택에 저장된 개수
+	int Count()
+	{
+		return m_Count;
+	}
+
+	// 저장된 데이터가 없는가?
+	bool IsEmpty()
+	{
+		return 0 == m_Count ? true : false;
+	}
+
+	// 데이터를 담을 수 있는 최대 개수
+	int GetStackSize()
+	{
+		return m_Size;
+	}
+
+	// 데이터를 저장한다.
+	bool push(T data)
+	{
+		// 저장할 수 있는 개수를 넘는지 조사한다.
+		if (m_Count >= m_Size)
+		{
+			return false;
+		}
+
+		// 저장 후 개수를 하나 늘린다.
+		m_aData[m_Count] = data;
+		++m_Count;
+
+		return true;
+	}
+
+	// 스택에서 빼낸다.
+	T pop()
+	{
+		// 저장된 것이 없다면 0을 반환한다.
+		if (m_Count < 1)
+		{
+			return 0;
+		}
+
+		// 개수를 하나 감소 후 반환한다.
+		--m_Count;
+		return m_aData[m_Count];
+
+	}
+
+private:
+	T* m_aData;
+	int m_Count;
+
+	int m_Size;
+};
+
+#include<string>
+// 아래의 코드는 문자열을 저장하기 위해 char* 으로 전문화한 Stack클래스입니다.
+template<> class Stack<char*>
+{
+public:
+	explicit Stack(int size)
+	{
+		m_Size = size;
+
+		m_ppData = new char* [m_Size];
+		for (int i = 0; i < m_Size; ++i)
+		{
+			m_ppData[i] = new char[MAX_ID_LENGTH];
+		}
+
+		Clear();
+	}
+
+	~Stack()
+	{
+		for (int i = 0; i < m_Size; ++i)
+		{
+			delete[] m_ppData[i];
+		}
+
+		delete[] m_ppData;
+	}
+
+	// 초기화 한다.
+	void Clear()
+	{
+		m_Count = 0;
+	}
+
+	// 스택에 저장된 개수
+	int Count()
+	{
+		return m_Count;
+	}
+
+	// 저장된 데이터가 없는가? 
+	bool IsEmpty()
+	{
+		return 0 == m_Count ? true : false;
+	}
+
+	// 데이터를 담을 수 있는 최대 개수
+	int GetStackSize()
+	{
+		return m_Size;
+	}
+
+	// 데이터를 저장한다.
+	bool push(char* pID)
+	{
+		// 저장할 수 있는 개수를 넘는지 조사한다.
+		if (m_Count >= m_Size)
+		{
+			return false;
+		}
+
+		// 저장 후 개수를 하나 늘린다.
+		strncpy_s(m_ppData[m_Count], MAX_ID_LENGTH, pID, MAX_ID_LENGTH - 1);
+		m_ppData[m_Count][MAX_ID_LENGTH - 1] = '\0';
+
+		++m_Count;
+
+		return true;
+	}
+
+	// 스택에서 빼낸다.
+	char* pop()
+	{
+		// 저장된 것이 없다면 0을 반환한다.
+		if (m_Count < 1)
+		{
+			return 0;
+		}
+
+		// 개수를 하나 감소 후 반환한다.
+		--m_Count;
+		return m_ppData[m_Count];
+	}
+
+private:
+	char** m_ppData;
+	int m_Count;
+	
+	int m_Size;
+};
+
+#include<iostream>
+using namespace std;
+
+void main()
+{
+	Stack<int> kStack1(64);
+	cout << "스택의 크기는?" << kStack1.GetStackSize() << endl;
+	kStack1.push(10);
+	kStack1.push(11);
+	kStack1.push(12);
+
+	int Count1 = kStack1.Count();
+	for (int i = 0; i < Count1; ++i)
+	{
+		cout << "유저의 레벨 변화 -> " << kStack1.pop() << endl;
+	}
+
+	cout << endl;
+
+	char GameID1[MAX_ID_LENGTH] = "NiceChoi";
+	char GameID2[MAX_ID_LENGTH] = "SuperMan";
+	char GameID3[MAX_ID_LENGTH] = "Attom";
+
+	// Stack 클래스 템플릿의 char* 전문화 버전을 생성한다.
+	Stack<char*> kStack2(64);
+	kStack2.push(GameID1);
+	kStack2.push(GameID2);
+	kStack2.push(GameID3);
+
+	int Count2 = kStack2.Count();
+	for (int i = 0; i < Count2; ++i)
+	{
+		cout << "같이 게임을 한 유저의 ID ->" << kStack2.pop() << endl;
+	}
 
 
+}
 // */
-
-
-
 
 
 
